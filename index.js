7e3689dac81ebd1409ff23629ffd5e146ef793b9
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-07-21 19:28:38 +0200

    new host

--- index.js
+++ index.js
@@ -9,19 +9,6 @@ export default (app, { getRouter }) => {
   const router = getRouter("/issue");
   router.use(express.static("public"));
 
-  app.log.info("Yay, the app was loaded!");
-
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
-  });
-
-  router.get("/hello-world", (req, res) => {
-    res.send("Hello World");
-  });
-
   router.post("/:owner/:repo", async (req, res) => {
     try {
       const { owner, repo } = req.params;
