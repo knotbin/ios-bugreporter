967481a1769dbac17467cc8ed38817b79a31f1bd
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-07-22 14:37:47 +0200

    context

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
 /**
  * This is the main entrypoint to your Probot app
- * 
  * @param {import('probot').Probot} app
+ * @param {import('probot').Context} context
  */
 import * as express from "express";
-import { ProbotOctokit } from "probot";
 
-export default (app, { getRouter }) => {
-  const octokit = new ProbotOctokit({
-    auth: {
-      id: process.env.APP_ID,
-      privateKey: process.env.PRIVATE_KEY
-    }
-  })
+export default (app) => {
   const router = getRouter("/issue");
   router.use(express.static("public"));
+  // Your code here
+  app.log.info("Yay, the app was loaded!");
 
-  router.post("/new-bug", async (req, res) => {
+  router.post("/new", async (req, res) => {
     try {
       const issue = {
-        owner: req.query.owner,
-        repo: req.query.repo,
         title: req.query.title,
         body: req.query.body
       };
-
-      createIssue(issue, octokit)
+      context.repo.owner = req.query.owner
+      context.repo.repo = req.query.repo
+      const { owner = req.query.owner, repo = req.query.repo } = context.repo()
+      return context.createIssue(title, body)
     } catch (error) {
       app.log.error(error);
-      res.status(500).json({ error: "Failed to create issue" });
+      res.status(500).json({ error: `Failed to create issue in repo ${repo}` });
     }
   });
+
+  app.on("issues.opened", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue!",
+    });
+    return context.octokit.issues.createComment(issueComment);
+  });
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
 };
-const createIssue = async function (issue, octokit) {
+
+const createIssue = async function (issue, app) {
+  const github = context.octokit.app.createIssue()
   const owner = issue.owner; const repo = issue.repo; const title = issue.title; const body = issue.body; const assignees = issue.assignees; const labels = issue.labels
-  octokit.issues.createIssue({ owner, repo, title, body, labels, assignees })
+  return github.issues.create({ owner, repo, title, body, labels, assignees })
 }
